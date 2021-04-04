d45031059d46f0f50832c44bb2e5dd50dffc49e2
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-04-04 18:44:37 +0900

    Revert "finished section 1"
    This reverts commit d104544f161d03e587b5a688da26a25b9017a98d.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
-Route::get('/', [ArticleController::class, 'index']);
